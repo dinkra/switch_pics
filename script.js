2f74c9ebcdf269490535c49a669a2574bd63f893
dinkra <dinarochka@gmail.com>
2014-12-08 21:14:05 +0100

    Page where I can switch pics

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
    var $str = 7;
    $('#button').click(function() {
 	   $str++;
-       $('#item_button').before("<li><img class='small_img' src='http://lorempixel.com/600/400/city/" + $str + "'/></li>");
+       $('#item_button').before("<li><img class='small_img' src='images/pic" + $str + '.jpeg' + "'/></li>");
    });
    var $last_src = $('#last_img').attr('src'); 
    $last_src =+ 1;
 });
 
+
+
+
+"<li><img class='small_img' src='himages/pic" + $str + '.jpeg' + "'/></li>"
+
+"<li><img class='small_img' src='http://lorempixel.com/600/400/city/" + $str + "'/></li>"
